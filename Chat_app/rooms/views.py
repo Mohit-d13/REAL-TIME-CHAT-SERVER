dec889573f4b3695830b1f209ed66376e4033e1d
Mohit-d13 <mohitdamle14@gmail.com>
2025-04-08 00:37:44 +0530

    Chat Group and Chat Messages Functionality

--- Chat_app/rooms/views.py
+++ Chat_app/rooms/views.py
@@ -1,11 +1,24 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from .models import Room, ChatMessage
+from .forms import RoomForm
 
 @login_required
 def chatgroup(request):
     chatgroups = Room.objects.all()
-    return render(request, 'rooms/chatgroup.html', {"chatgroups": chatgroups})
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    else:
+        form = RoomForm()
+        
+    return render(request, 'rooms/chatgroup.html', {
+        "chatgroups": chatgroups,
+        "form": form
+        })
 
 @login_required
 def chat(request, slug):
