509548f9d78ede9846f19558989b92f55df17088
Mohit-d13 <mohitdamle14@gmail.com>
2025-04-07 19:40:56 +0530

    Signup, Login and Profile Form

--- Chat_app/core/forms.py
+++ Chat_app/core/forms.py
@@ -9,27 +9,68 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+        widgets = {
+            "username": forms.TextInput(attrs={
+                "placeholder": "Enter your Username",
+                "class": "form-control"
+            })
+        }
+    
+    # The password fields are defined on the form class itself, not on the model.
+    # So override them to set widget attributes  
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+            
+        self.fields['password1'].widget = forms.PasswordInput(attrs={
+            "placeholder": "Enter your Password",
+            "class": "form-control"
+        })
+            
+        self.fields['password2'].widget = forms.PasswordInput(attrs={
+            "placeholder": "Confirm Password",
+            "class": "form-control"
+        })
         
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name','last_name','email']
+        fields = ['username','first_name','last_name','email']
+        widgets = {
+            "username": forms.TextInput(attrs={
+                "class": "form-control"
+            }),
+            "first_name": forms.TextInput(attrs={
+                "class": "form-control"
+            }),
+            "last_name": forms.TextInput(attrs={
+                "class": "form-control"
+            }),
+            "email": forms.EmailInput(attrs={
+                "class": "form-control"
+            })
+        }
         
-class ProfileForm(forms.ModelForm):
+class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['birth_date', 'picture']
-        
-        
-LOGIN_SYTLE_CLASS = "form-control p-2 mt-2"
+        widgets = {
+            "birth_date": forms.DateInput(attrs={
+            "class": "form-control",
+            "type": "date"
+            }),
+            "picture": forms.FileInput(attrs={
+            "class": "form-control"
+            })
+        }
       
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         "placeholder": "Enter your Username",
-        "class": LOGIN_SYTLE_CLASS,
+        "class": "form-control"
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         "placeholder": "Enter your Password",
-        "class": LOGIN_SYTLE_CLASS,
+        "class": "form-control"
     }))
         
